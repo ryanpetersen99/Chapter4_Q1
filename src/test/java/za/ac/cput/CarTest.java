c7cac31e0fa2c3fa32c75770097524e34ef56a9a
ryanpetersen99 <47520518+ryanpetersen99@users.noreply.github.com>
2019-03-16 21:15:15 +0200

    Initial commit

---
+++ src/test/java/za/ac/cput/CarTest.java
@@ -0,0 +1,25 @@
+package za.ac.cput;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CarTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void Test()
+    {
+        Honda honda = new Honda(0,0,"null");
+        assertEquals(120,honda.getSpeed());
+        assertEquals(5,honda.getGears());
+        assertEquals("Honda",honda.getModel());
+
+    }
+}
